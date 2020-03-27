a4b1e77683995b29c36e98e09ad3f1ef65c3e9e6
caifu92 <caifu5340@gmail.com>
2020-03-27 21:41:15 +0800

    #1: imported ListTable into Dashboard

--- src/dashboard/index.js
+++ src/dashboard/index.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import { NavigationBar } from '../common/components/navigationBar';
+import ListTable from '../common/components/listTable';
 
 export const Dashboard = (props) => {
   return (
-    <NavigationBar />
+    <div>
+      <NavigationBar />
+
+      <div style={{ height: '50px' }}>
+        {/* TODO: replace with filter */}
+      </div>
+
+      <ListTable />
+    </div>
   );
 };
