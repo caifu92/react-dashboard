1e6cfa363ea4be7efd1c98aefd65d2e48d0055c3
caifu92 <caifu5340@gmail.com>
2020-03-27 21:13:19 +0800

    #1: Updated App.jsx with a temporary route for /dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
+import { Dashboard } from './dashboard';
 
 const App = () => {
   return (
     <Switch>
       <Route exact path="/" component={() => <div>BASE ROUTE</div>} />
+      <Route exact path="/dashboard" component={() => <Dashboard />} />
     </Switch>
   );
 };
