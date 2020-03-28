770bb197d47c1c2a650be99bffb9d60d0d1325b6
caifu92 <caifu5340@gmail.com>
2020-03-29 07:43:01 +0800

    Improved UI based on mockup

--- src/dashboard/pages/denyModal/DenyModal.js
+++ src/dashboard/pages/denyModal/DenyModal.js
@@ -1,10 +1,54 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import { Button, Dialog, DialogTitle, DialogContent, DialogActions, TextField } from '@material-ui/core';
+import { Colors } from '../../../common/constants/Colors';
 
 const useStyles = makeStyles(theme => ({
-  center: {
-   textAlign: 'center'
+  modalTitle: {
+    borderBottom: '0.5px solid',
+    textAlign: 'center'
+  },
+  modalContent: {
+   alignItems: 'center',
+   textAlign: 'center',
+   width: 350
+  },
+  modalText: {
+    margin: '20px auto 20px auto',
+  },
+  actions: {
+    flex: 1,
+    flexDirection: 'row',
+    display: 'inline-flex',
+    margin: '20px auto 20px auto',
+    '& .MuiButton-root': {
+      width: 157,
+      fontSize: 14,
+      color: Colors.White,
+      '&:hover': {
+        opacity: 0.9,
+        textShadow: '0 -1px 1px #5f5f5f, 0 -1px 1px #fff'
+      }
+    },
+    '& .MuiButtonBase-root': {
+      textTransform: 'capitalize',
+      borderRadius: 20,
+      fontWeight: 600,
+      fontFamily: 'Work Sans, sans-serif',
+      margin: 4,
+    }
+  },
+  denyAction: {
+    backgroundColor: Colors.DenialRed,
+    '&:hover': {
+      backgroundColor: Colors.DenialRed
+    }
+  },
+  cancelAction: {
+    backgroundColor: Colors.CancelGray,
+    '&:hover': {
+      backgroundColor: Colors.CancelGray
+    }
   },
 }));
 
@@ -14,10 +58,10 @@ export const DenyApplicationModal = ({show, closeModal}) => {
 
   return (
     <Dialog open={show} onClose={closeModal}>
-      <DialogTitle>Deny Application?</DialogTitle>
+      <DialogTitle className={classes.modalTitle}>Deny Application?</DialogTitle>
       <DialogContent>
-        <div className={classes.center}>
-          Are you sure you want to DENY APPLICATION?<br /><br />
+        <div className={classes.modalContent}>
+          <div className={classes.modalText}>Are you sure you want to<br />DENY APPLICATION?</div>
           <div>
             <TextField
               label="Remarks"
@@ -30,9 +74,9 @@ export const DenyApplicationModal = ({show, closeModal}) => {
           </div>
         </div>
       </DialogContent>
-      <DialogActions>
-        <Button variant="contained" color="secondary" onClick={closeModal}>YES, DENY</Button>
-        <Button variant="contained" onClick={closeModal}>CANCEL</Button>
+      <DialogActions className={classes.actions}>
+        <Button variant="contained" className={classes.denyAction} onClick={closeModal}>YES, DENY</Button>
+        <Button variant="contained" className={classes.cancelAction} onClick={closeModal}>CANCEL</Button>
       </DialogActions>
     </Dialog>
   );
