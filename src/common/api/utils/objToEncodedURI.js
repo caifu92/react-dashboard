731e6fc0ca762d5f7f384713e9e808caff8f80dc
caifu92 <caifu5340@gmail.com>
2020-04-11 16:36:56 +0800

    refactor: adjust `objToEncodedURI()` implementation
    BREAKING CHANGE: `objToEncodedURI()` omits query query strings with `undefined` value

--- src/common/api/utils/objToEncodedURI.js
+++ src/common/api/utils/objToEncodedURI.js
@@ -1,8 +1,12 @@
 export const objToEncodedURI = (obj) => {
-  return Object.entries(obj)
-    .map(
-      ([paramKey, paramValue]) =>
-        `${encodeURIComponent(paramKey)}=${encodeURIComponent(paramValue)}`
-    )
-    .join('&');
+  return (
+    Object.entries(obj)
+      // eslint-disable-next-line no-unused-vars
+      .filter(([paramKey, paramValue]) => typeof paramValue !== 'undefined')
+      .map(
+        ([paramKey, paramValue]) =>
+          `${encodeURIComponent(paramKey)}=${encodeURIComponent(paramValue)}`
+      )
+      .join('&')
+  );
 };
