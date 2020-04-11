13d06c92e3a69613c1411f30158ca640c9974f19
caifu92 <caifu5340@gmail.com>
2020-04-11 12:38:13 +0800

    #56: updated template link to default to public Gitlab absolute URL. can be overriden with sourceUrl prop for DownloadTemplateLink

--- src/bulkUpload/passUploadTab/DownloadTemplateLink.js
+++ src/bulkUpload/passUploadTab/DownloadTemplateLink.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import { Typography, Link, makeStyles } from '@material-ui/core';
-
+const GITLAB_PUBLIC_URL = `https://gitlab.com/dctx/rapidpass/rapidpass-dashboard/-/tree/master/public`;
 const useStyles = makeStyles({
   linkText: {
     paddingLeft: 5,
   },
 });
 
-export const DownloadTemplateLink = () => {
+export const DownloadTemplateLink = ({ sourceURL = GITLAB_PUBLIC_URL }) => {
   const classes = useStyles();
   return (
     <Typography>
       Download the template
-      <Link href="/templates/bulk-upload-template.csv" download className={classes.linkText}>
+      <Link href={`${sourceURL}/templates/bulk-upload-template.csv`} download className={classes.linkText}>
         here.
       </Link>
     </Typography>
