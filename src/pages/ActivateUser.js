31609215441a9e9f11327ffd24c34f260f3ab1f5
caifu92 <caifu5340@gmail.com>
2020-04-11 12:03:53 +0800

    #87: moved rules validation to password, so that confirmPassword will merely attempt to match. made named constants for regex and min length. TODO: remove the min count 11 in the regex? (11 matches 12 total chars there)

--- src/pages/ActivateUser.js
+++ src/pages/ActivateUser.js
@@ -11,34 +11,26 @@ import { Colors } from '../common/constants/Colors';
 import { CenteredForm } from '../common/components/CenteredForm';
 import { useActivateApprover } from '../common/hooks/useActivateApprover';
 
+const MIN_LENGTH = 12;
+/** At least 12, at least 1 upper, 1 lower, 1 symbol. */
+// TODO remove the 11 value - already checked by min()?
+const REGEX_UPPER_LOWER_ALPHANUMERIC = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).(?=.*[#$^+=!*()@%&]).{11,}$/;
+
 const validationSchema = yup.object({
-  password: yup.string().required('Password is required'),
+  password: yup
+    .string()
+    .required('Please confirm your password.')
+    .min(MIN_LENGTH, `At least ${MIN_LENGTH} characters.`)
+    .matches(
+      REGEX_UPPER_LOWER_ALPHANUMERIC,
+      `Please at least one of each: an uppercase letter, a lowercase letter,  a number, and a symbol.`),
   confirmPassword: yup.string().when('password', {
-    is: (val) => val && val.length > 0,
+    is: (val) => val && val.length >= MIN_LENGTH,
     then: yup
       .string()
-      .oneOf([yup.ref('password')], "Password Doesn't Match")
-      .required('Please confirm your password.')
-      .min(12, 'Password must be at least 12-character long')
-      .matches(
-        /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).(?=.*[#$^+=!*()@%&]).{12,}$/,
-        'Kindly follow the password requirements:\n' +
-          'English alphabet uppercase letter (A-Z),\n' +
-          'English alphabet lowercase letter (a-z),\n' +
-          'Decimal digit number (0-9), AND\n' +
-          'Symbols (#$^+=!*()@%&)'
-      ),
+      .oneOf([yup.ref('password')], "Please confirm your chosen password.")
   }),
 });
-
-const formatError = (error) =>
-  error.split('\n').map((text) => (
-    <React.Fragment key={`${text}`}>
-      {text}
-      <br />
-    </React.Fragment>
-  ));
-
 export const ActivateUser = () => {
   const location = useLocation();
   const { push } = useHistory();
@@ -130,6 +122,7 @@ export const ActivateUser = () => {
             variant="outlined"
             value={values.confirmPassword}
             onChange={handleChange}
+            helperText={errors.confirmPassword}
             error={!!errors.confirmPassword}
             disabled={isLoading}
           />
@@ -149,13 +142,6 @@ export const ActivateUser = () => {
           </ErrorMessage>
         )}
 
-        {!!errors.confirmPassword && (
-          <ErrorMessage>
-            <ErrorIcon />
-            <span>{formatError(errors.confirmPassword)}</span>
-          </ErrorMessage>
-        )}
-
         <SubmitButton
           type="submit"
           variant="contained"
@@ -169,6 +155,8 @@ export const ActivateUser = () => {
     </CenteredForm>
   );
 };
+
+
 
 const FormField = styled(TextField)({
   width: '100%',
