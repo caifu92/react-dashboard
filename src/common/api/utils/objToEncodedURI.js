6475971465c9d686c673f038c790c80568da418d
caifu92 <caifu5340@gmail.com>
2020-04-12 16:54:19 +0800

    :fire: - Removed lint

--- src/common/api/utils/objToEncodedURI.js
+++ src/common/api/utils/objToEncodedURI.js
@@ -1,12 +1,9 @@
 export const objToEncodedURI = (obj) => {
-  return (
-    Object.entries(obj)
-      // eslint-disable-next-line no-unused-vars
-      .filter(([paramKey, paramValue]) => typeof paramValue !== 'undefined')
-      .map(
-        ([paramKey, paramValue]) =>
-          `${encodeURIComponent(paramKey)}=${encodeURIComponent(paramValue)}`
-      )
-      .join('&')
-  );
+  return Object.entries(obj)
+    .filter(([, paramValue]) => typeof paramValue !== 'undefined')
+    .map(
+      ([paramKey, paramValue]) =>
+        `${encodeURIComponent(paramKey)}=${encodeURIComponent(paramValue)}`
+    )
+    .join('&');
 };
