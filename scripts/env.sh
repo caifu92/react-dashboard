f5ee8f9c33ad238bee6c5c3dcd6f142e4276eb90
caifu92 <caifu5340@gmail.com>
2020-04-12 18:19:29 +0800

    CI: adde apiKey to env.sh

--- scripts/env.sh
+++ scripts/env.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
 apiUrl="${REACT_APP_API_URL:-https://api.test.rapidpass.amihan.net/api}"
+apiKey="${REACT_APP_API_KEY:-unset}"
 cat <<EOF
 window.REACT_APP_API_URL='$apiUrl';
+window.REACT_APP_API_KEY='$apiKey';
 EOF
