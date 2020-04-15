2e5e247cbcf8a982c7a9029a865c95096c8f4622
caifu92 <caifu5340@gmail.com>
2020-04-15 11:10:54 +0000

    #96: possible fix. made auth token flaggable to be skipped like for using for public endpoints like /auth.

--- scripts/env.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-apiUrl="${REACT_APP_API_URL:-https://rapidpass-api-stage.azurewebsites.net/api}"
-apiKey="${REACT_APP_API_KEY:-unset}"
-cat <<EOF
-window.REACT_APP_API_URL='$apiUrl';
-window.REACT_APP_API_KEY='$apiKey';
-EOF
