4a15fab4254a4c8afb0a2b58e32752e631ca3e76
caifu92 <caifu5340@gmail.com>
2020-04-19 15:50:02 +0800

    #106: revised NavigationBar to support new UserMenu component (wraps buttons into popover menu). TODO: integrate with 107 change password route and component

---
+++ src/common/components/navigationBar/UserMenu.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import {
+  Button,
+  IconButton,
+  Menu,
+  MenuItem,
+  Fade,
+  Typography
+} from '@material-ui/core';
+import SettingsIcon from '@material-ui/icons/Settings';
+import KeyboardArrowDownIcon from '@material-ui/icons/KeyboardArrowDown';
+
+function UserMenu({ username, children }) {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+  const handleOpenMenu = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleCloseMenu = () => {
+    setAnchorEl(null);
+  };
+
+  return <>
+    <IconButton
+      edge="end"
+      color="inherit"
+      aria-label="menu"
+      aria-controls="user-menu"
+      aria-haspopup="true"
+      onClick={handleOpenMenu}>
+      <SettingsIcon />
+      <Typography variant="body1"> {username} </Typography>
+      <KeyboardArrowDownIcon />
+    </IconButton>
+    <Menu
+      id="user-menu"
+      anchorEl={anchorEl}
+      getContentAnchorEl={null}
+      anchorOrigin={{ vertical: "bottom", horizontal: "center" }}
+      transformOrigin={{ vertical: "top", horizontal: "center" }}
+      keepMounted
+      open={open}
+      onClose={handleCloseMenu}
+      TransitionComponent={Fade}
+    >
+      {React.Children.map(children, (child) =>
+        <MenuItem onClick={handleCloseMenu}>{child}</MenuItem>
+      )}
+    </Menu>
+  </>
+}
+
+UserMenu.propTypes = {
+  username: PropTypes.string.isRequired,
+  children: PropTypes.node.isRequired
+}
+
+export default UserMenu;
