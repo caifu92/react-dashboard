a8e65ec14ae162b1906adcf8d02f33ac6e5aa396
caifu92 <caifu5340@gmail.com>
2020-04-26 14:44:07 +0800

    CI: initial working implementation of react-unleash-flags. TODO: rewrap in separate HOC

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Provider } from 'react-redux';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import { MuiThemeProvider } from '@material-ui/core/styles';
 import { PersistGate } from 'redux-persist/integration/react';
+import { FeatureFlag } from 'react-unleash-flags';
 
 import { theme } from './theme';
 import createHistory from './history';
@@ -12,26 +13,31 @@ import { AppRoutes } from './AppRoutes';
 import { SnackbarProvider } from './context';
 import { Maintenance } from './pages/Maintenance';
 
+
 export function App() {
   const history = createHistory();
 
-  if (Number(process.env.REACT_APP_MAINTENANCE)) {
-    return <Maintenance />;
-  }
-
   return (
-    <Provider store={store}>
-      <PersistGate persistor={persistor}>
-        <Router history={history}>
-          <MuiThemeProvider theme={theme}>
-            <SnackbarProvider>
-              <AppRoutes history={history} />
-            </SnackbarProvider>
-            <CssBaseline />
-          </MuiThemeProvider>
-        </Router>
-      </PersistGate>
-    </Provider>
+    <>
+      <FeatureFlag name="maintenance-development">
+        <Maintenance />
+      </FeatureFlag>
+
+      <FeatureFlag name="maintenance-development" invert={true}>
+        <Provider store={store}>
+          <PersistGate persistor={persistor}>
+            <Router history={history}>
+              <MuiThemeProvider theme={theme}>
+                <SnackbarProvider>
+                  <AppRoutes history={history} />
+                </SnackbarProvider>
+                <CssBaseline />
+              </MuiThemeProvider>
+            </Router>
+          </PersistGate>
+        </Provider>
+      </FeatureFlag>
+    </>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,15 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { FlagsProvider } from 'react-unleash-flags';
 
 import { App } from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const root = document.getElementById('root');
+
+// we can also define the config in code instead of using env vars
+const flagConfig = {
+  appName: process.env.NODE_ENV,
+  url: 'https://gitlab.com/api/v4/feature_flags/unleash/17749136',
+  instanceId: 'ruL9XsszeoKcFRbAwz9S',
+};
+
+if (!!root) {
+  ReactDOM.render(
+    <React.StrictMode>
+      <FlagsProvider config={flagConfig}>
+        <App />
+      </FlagsProvider>
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
+}
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
