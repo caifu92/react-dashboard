25ccc5f221bf4eb492ff90ba1f30dbc337bf6974
caifu92 <caifu5340@gmail.com>
2020-04-26 10:09:57 +0000

    CI: gitlab-ci.yml updated with build, test, deploy, production stages (https://docs.gitlab.com/ee/ci/yaml/#stages). * develop - builds to dev env; on success, will build to test env * release/ - builds to staging * master - manual-triggered build to prod env

--- src/pages/Maintenance.js
+++ src/pages/Maintenance.js
@@ -1,26 +1,22 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import { Box, Typography } from '@material-ui/core';
+import { Typography } from '@material-ui/core';
+import { MuiThemeProvider } from '@material-ui/core/styles';
+import BuildIcon from '@material-ui/icons/Build';
 
-import MainLogo from '../common/components/MainLogo';
+import { CenteredForm } from '../common/components/CenteredForm';
+import { theme } from '../theme';
 
-const useStyles = makeStyles({
-  container: {
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-    height: '100vh',
-    flexDirection: 'column',
-  },
-});
 
 export const Maintenance = () => {
-  const classes = useStyles();
   return (
-    <Box className={classes.container}>
-      <MainLogo />
-      <Typography>Maintenance Mode</Typography>
-    </Box>
+    <MuiThemeProvider theme={theme}>
+      <CenteredForm formTitle=" Maintenance Mode" showLogo>
+        <Typography variant="body1" color="textSecondary">
+          We'll be back soon.
+      </Typography>
+        <BuildIcon fontSize="large" color="primary" />
+      </CenteredForm>
+    </MuiThemeProvider>
   );
 };
 
