fe273bf32e2a15218c38daf7f43d9075d78f6a36
caifu92 <caifu5340@gmail.com>
2020-04-26 17:07:12 +0800

    CI: reenable optional feature flag (dont implement if incomplete config

--- src/common/components/FeatureToggle.js
+++ src/common/components/FeatureToggle.js
@@ -1,38 +1,55 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { FeatureFlag } from 'react-unleash-flags';
+import { FeatureFlag, FlagsProvider } from 'react-unleash-flags';
 
-export const serverEnv = process.env.REACT_APP_ENV || process.env.NODE_ENV;
+export const serverEnv = process.env.REACT_APP_ENV || '';
 export const flagConfig = {
   appName: serverEnv,
   url: 'https://gitlab.com/api/v4/feature_flags/unleash/17749136',
   instanceId: 'swfN4v8HT29sNLj9Z2mK',
 };
 
-/**
+const isFeatureFlagUsable = flagConfig.url && flagConfig.instanceId && serverEnv;
+
+export const FeatureFlagsProvider = ({ children }) => {
+  // disable feature flag checking if not configured? non-strict
+  return isFeatureFlagUsable ? (
+    <FlagsProvider config={flagConfig}>{children}</FlagsProvider>
+  ) : (
+    <>{children}</>
+  );
+};
+
+FeatureFlagsProvider.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+/*
  * Powered by Gitlab CI Feature Flags
  * Toggles two nodes (children, fallback) depending on the toggle
  * in Gitlab > Operations > Feature Flags
-*/
+ */
 export const FeatureToggle = ({ featureKey, environment: env, children, fallback }) => {
   const feature = featureKey + (env ? `-${env}` : ''); // naming convention
   // when env is not provided, this feature key will affect all servers at once.
-  return (
+  return !isFeatureFlagUsable ? (
+    <>{children}</>
+  ) : (
     <>
-      <FeatureFlag name={feature}>
-        {children}
-      </FeatureFlag>
+      <FeatureFlag name={feature}>{children}</FeatureFlag>
 
-      {fallback && <FeatureFlag name={feature} invert={true}>
-        {fallback}
-      </FeatureFlag>}
+      {fallback && (
+        <FeatureFlag name={feature} invert>
+          {fallback}
+        </FeatureFlag>
+      )}
     </>
   );
-}
+};
 
 FeatureToggle.propTypes = {
   featureKey: PropTypes.string.isRequired,
   children: PropTypes.node.isRequired,
   fallback: PropTypes.node,
   environment: PropTypes.string,
-}
+};
