851298838970e20b048a16d9878dd226dda8e689
caifu92 <caifu5340@gmail.com>
2020-05-08 22:29:33 +0800

    check application status route added

--- src/common/components/navigationBar/ProtectedRoutes.js
+++ src/common/components/navigationBar/ProtectedRoutes.js
@@ -1,6 +1,7 @@
 import { Dashboard } from '../../../dashboard/pages/Dashboard';
 import { BulkUpload } from '../../../bulkUpload/BulkUpload';
 import { ChangePassword } from '../../../pages/ChangePassword';
+import { CheckApplicationStatus } from '../../../dashboard/pages/CheckApplicationStatus';
 import { KeycloakRoles } from '../../constants';
 
 export const PROTECTED_ROUTES = [
@@ -10,7 +11,7 @@ export const PROTECTED_ROUTES = [
     title: 'Applications',
     component: Dashboard,
     show: true,
-    role: KeycloakRoles.HAS_VIEW_DETAILS_ACCESS, //permission
+    role: KeycloakRoles.HAS_VIEW_DETAILS_ACCESS, // permission
   },
   {
     path: '/bulk-upload',
@@ -19,6 +20,14 @@ export const PROTECTED_ROUTES = [
     component: BulkUpload,
     show: true,
     role: KeycloakRoles.HAS_BULK_UPLOAD_ACCESS,
+  },
+  {
+    path: '/check-status',
+    exact: true,
+    title: 'Check Status',
+    component: CheckApplicationStatus,
+    show: true,
+    role: KeycloakRoles.HAS_CHECK_APPLICATION_STATUS,
   },
   {
     path: '/change-password',
