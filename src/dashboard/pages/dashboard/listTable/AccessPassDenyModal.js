1b111832dfd0b24fc5024ae3e718b170890c97c0
caifu92 <caifu5340@gmail.com>
2020-05-19 20:59:25 +0800

    Input props update blocking code style

--- src/dashboard/pages/dashboard/listTable/AccessPassDenyModal.js
+++ src/dashboard/pages/dashboard/listTable/AccessPassDenyModal.js
@@ -119,9 +119,7 @@ export const AccessPassDenyModal = ({ value = {}, loading, isOpen, onClose, onSu
           values={formik.values.remarks}
           onChange={handleChange}
           disabled={loading}
-          inputProps={{
-            maxLength: 131,
-          }}
+          inputProps={{ maxLength: 131 }}
         />
       </DialogContent>
       <DialogActions className={classes.actions}>
